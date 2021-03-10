88c6b05454de731f5fbc2933091cd11db65c7ba1
jempico <jpfilarca@gmail.com>
2021-03-10 14:21:33 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const util = require('util');
-const fs = require('fs');
-const repeat = require('./repeat.js');
-const createfile = require('./createfile.js');
-const readfile = require('./readfile.js');
-
-repeat('Keep coding!');
-createfile('Jemimah');
-readfile('./input.txt');
